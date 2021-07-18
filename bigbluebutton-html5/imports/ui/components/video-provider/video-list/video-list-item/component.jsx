30fb44da09464694946c7a7b46b9bd726ce042ea
miguelGU <miguel.guev@outlook.com>
2021-07-18 16:55:51 -0400

    Added onclick func on webcamitem

--- bigbluebutton-html5/imports/ui/components/video-provider/video-list/video-list-item/component.jsx
+++ bigbluebutton-html5/imports/ui/components/video-provider/video-list/video-list-item/component.jsx
@@ -199,6 +199,7 @@ class VideoListItem extends Component {
           [styles.content]: true,
           [styles.talking]: voiceUser.talking,
         })}
+        onClick={() => FullscreenService.toggleFullScreen(ref)}
       >
         {
           !videoIsReady
